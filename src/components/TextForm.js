fc330552163cabeee7e7096266fdca24ed802c71
AvinashSutariya <avinash.s@simformsolutions.com>
2022-07-09 08:36:21 +0530

    Adding more Logic to TextUtils | #8

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,29 +5,48 @@ export default function TextForm(props) {
   const handleUpClick = () => {
     setText(text.toUpperCase());
   };
- 
+
+  const handleLowerClick = () => {
+    setText(text.toLowerCase());
+  };
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
 
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState('');
 
+  let wordCout = text.split(" ").length;
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="12"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="12"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          ToUpperCase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLowerClick}>
+          ToLowerCase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        ToUpperCase
-      </button>
-    </div>
+      <div className="container my-5">
+        <h2>Your text summary</h2>
+        <p>
+          {wordCout} words, {text.length} characters
+        </p>
+        <p>{0.008 * wordCout} minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
 
